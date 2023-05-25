4f771f5adc1abe04fc7a308b698b8ace1ad67e48
rayzAqua <hieulcgl@gmail.com>
2023-05-25 21:34:45 +0700

    "Fix bug"

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getCustomerList, createCustomer } from "../controllers/customer_controller.js";
+import { authenticateToken } from "../middlewares/authentication.js";
 
 const router = express.Router();
 
 // CREATE
-router.post("/add", createCustomer);
+router.post("/add", authenticateToken, createCustomer);
 
 // GET FOOD LIST
 router.get("/list", getCustomerList);
